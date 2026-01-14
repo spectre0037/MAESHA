428b079b10e96a984c5cd8f9de8e245da2360318
Abdullah Bin Zubair <u2023037@giki.edu.pk>
2026-01-14 19:49:37 +0500

    updated

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -12,7 +12,7 @@ app.use(helmet({
   crossOriginResourcePolicy: false, // Allows images to load on frontend
 })); 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'www.maesha.store',
+  origin:'https://www.maesha.store',
   credentials: true
 }));
 app.use(morgan('dev')); 
